2e4ef3e90b9100799ed01e2bced1276bfc5b8f3d
Rupotron <dasrup4545@gmail.com>
2026-02-27 20:36:35 +0530

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,45 +1,62 @@
-function generateID() {
-    // Populate Data
-    document.getElementById('displayName').innerText = document.getElementById('inName').value;
-    document.getElementById('displayRole').innerText = document.getElementById('inRole').value;
-    document.getElementById('displayID').innerText = document.getElementById('inID').value;
+async function generateID() {
+    const name = document.getElementById('inName').value;
+    const idNum = document.getElementById('inID').value;
+    const photoInput = document.getElementById('inPhoto');
 
-    // Generate QR Code containing Employee Name and ID
+    if (!name || !idNum || !photoInput.files[0]) {
+        alert("Please fill in all fields and upload a photo.");
+        return;
+    }
+
+    // 1. Update Text
+    document.getElementById('displayName').innerText = name;
+    document.getElementById('displayRole').innerText = document.getElementById('inRole').value;
+    document.getElementById('displayID').innerText = idNum;
+
+    // 2. Generate QR Code
     const qrContainer = document.getElementById("qrcode");
     qrContainer.innerHTML = "";
     new QRCode(qrContainer, {
-        text: "Emp: " + document.getElementById('inName').value + " ID: " + document.getElementById('inID').value,
+        text: `Employee: ${name} | ID: ${idNum}`,
         width: 130,
-        height: 130
+        height: 130,
+        correctLevel: QRCode.CorrectLevel.H
     });
 
-    // Handle Photo Upload
-    const photoInput = document.getElementById('inPhoto');
+    // 3. Handle Photo and Trigger PDF
     const reader = new FileReader();
-    
     reader.onload = function(e) {
-        document.getElementById('displayPhoto').src = e.target.result;
-        
-        // Finalize PDF
-        const element = document.getElementById('id-template');
-        element.classList.remove('hidden-print');
-        
-        const opt = {
-            margin: 0,
-            filename: 'Astavinayak_ID_Card.pdf',
-            image: { type: 'jpeg', quality: 0.98 },
-            html2canvas: { scale: 3 },
-            jsPDF: { unit: 'px', format: [400, 600], orientation: 'portrait' }
+        const photoImg = document.getElementById('displayPhoto');
+        photoImg.src = e.target.result;
+
+        // Wait for photo to load visually before capturing
+        photoImg.onload = function() {
+            const element = document.getElementById('id-template');
+            element.classList.remove('hidden-print');
+
+            const opt = {
+                margin: 0,
+                filename: `ID_Card_${name.replace(/\s+/g, '_')}.pdf`,
+                image: { type: 'jpeg', quality: 1.0 },
+                html2canvas: { 
+                    scale: 3, 
+                    useCORS: true, // Crucial for loading images/logos
+                    logging: true 
+                },
+                jsPDF: { unit: 'px', format: [400, 600], orientation: 'portrait' }
+            };
+
+            // Generate the PDF
+            html2pdf().set(opt).from(element).save()
+                .then(() => {
+                    element.classList.add('hidden-print');
+                    console.log("Download successful");
+                })
+                .catch(err => {
+                    console.error("PDF Error:", err);
+                    alert("Failed to generate PDF. Check console for details.");
+                });
         };
-
-        html2pdf().set(opt).from(element).save().then(() => {
-            element.classList.add('hidden-print');
-        });
     };
-
-    if (photoInput.files[0]) {
-        reader.readAsDataURL(photoInput.files[0]);
-    } else {
-        alert("Please upload a photo first!");
-    }
+    reader.readAsDataURL(photoInput.files[0]);
 }
